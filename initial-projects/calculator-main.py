b67b97828eace7458deffc9c1ede7450d1d49293
codicescriptor <73770368+codicescriptor@users.noreply.github.com>
2021-05-20 12:18:40 +0530

    Create calculator-main.py

---
+++ initial-projects/calculator-main.py
@@ -0,0 +1,45 @@
+#Add
+def add(n1, n2):
+    return n1 + n2
+
+#Subtract
+def sub(n1, n2):
+    return n1- n2
+    
+#Multiply
+def mul(n1, n2):
+    return n1 * n2
+
+#Divide
+def div(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    "+": add,
+    "-": sub,
+    "*": mul,
+    "/": div
+}
+
+def calculator():
+    print(logo)
+    num1 = float(input("what's the first number? "))
+    for symbol in operations:
+        print(symbol)
+    ask_user = True
+    while ask_user:
+        op_sym1 = input("Pick an operation from above: ")
+        num2 = float(input("what's the next number? "))
+
+        calc = operations[op_sym1]
+        answer = calc(num1 , num2)
+        print(f"{num1} {op_sym1} {num2} = {answer}")
+
+        ask_user = input("Do u want to continue? type 'y' for yes or 'n' for new calculation : ")
+        if ask_user == "y":
+            num1 = answer
+        else:
+            ask_user = False
+            calculator() #recursion
+calculator()
