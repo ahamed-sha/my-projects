77c75403b53ccd1cd33b873c546c4a6241bc278a
kodscriptor <73770368+kodscriptor@users.noreply.github.com>
2021-05-24 23:11:32 +0530

    Add files via upload

---
+++ pong-game/main.py
@@ -0,0 +1,47 @@
+import time
+from paddle import Paddle
+from turtle import Turtle, Screen
+from ball import Ball
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Ping Pong Game")
+screen.tracer(0)  # By setting it to 0, we have to manually update the screen's animation by update() method
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(fun=r_paddle.move_up, key="Up")
+screen.onkey(fun=r_paddle.move_down, key="Down")
+screen.onkey(fun=l_paddle.move_up, key="w")
+screen.onkey(fun=l_paddle.move_down, key="s")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
+
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    if (ball.distance(r_paddle) < 50 and ball.xcor() > 320) or (ball.distance(l_paddle) < 50 and ball.xcor() < -320):
+        ball.bounce_x()
+
+    if ball.xcor() > 380:
+        ball.reset()
+        scoreboard.l_point()
+
+    if ball.xcor() < -380:
+        ball.reset()
+        scoreboard.r_point()
+
+
+
+
+screen.exitonclick()
