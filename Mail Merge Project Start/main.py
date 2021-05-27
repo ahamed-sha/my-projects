ae760ee18ee56e98dfad3ffd3d61a07817260c34
kodscriptor <73770368+kodscriptor@users.noreply.github.com>
2021-05-27 21:19:08 +0530

    Update main.py

--- Mail Merge Project Start/main.py
+++ Mail Merge Project Start/main.py
@@ -1,7 +1,3 @@
-# TODO: Create a letter using starting_letter.txt
-# for each name in invited_names.txt
-# Replace the [name] placeholder with the actual name.
-# Save the letters in the folder "ReadyToSend".
 name_list = []
 with open("./Input/Names/invited_names.txt") as names:
     list_of_names = names.readlines()
