39c576caa9fcebf361c9b8f7ade84973e573608b
Ahamed Sha <73770368+ahamed-sha@users.noreply.github.com>
2021-06-10 22:31:25 +0530

    Update main.py

--- rain-alert/main.py
+++ rain-alert/main.py
@@ -2,9 +2,9 @@ import requests
 from twilio.rest import Client
 
 endpoint = "https://api.openweathermap.org/data/2.5/onecall"
-api_key = "0b99b56c4a66e27da39a6d0a1c10cec6"
-account_sid = "AC85239be95f65bff30bcab4b8a73219e0"
-auth_token = "78186b3bedabede606f3ecbd5d277781"
+api_key = "131"
+account_sid = "2424424"
+auth_token = "424242424"
 
 weather_params = {
     'lat': 20.145929,
@@ -30,8 +30,8 @@ if will_rain:
     message = client.messages \
         .create(
         body="you probably need an umbrella!",
-        from_="+16149547082",
-        to='+919994838033'
+        from_="24242",
+        to='242424'
     )
 
     print(message.status)
