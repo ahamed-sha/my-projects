026520daff1ad858d1679c59c0a23369891bf0d2
Ahamed Sha <ahamedsha@outlook.in>
2021-06-15 20:49:06 +0530

    part-1 finished

--- flight-deals-start/notification_manager.py
+++ flight-deals-start/notification_manager.py
@@ -1,3 +1,15 @@
+import smtplib
+EMAIL = "test@gmail.com"
+PASSWORD = "1414414"
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+
+    def send_email(self,  message):
+        with smtplib.SMTP("smtp.gmail.com") as conn:
+            conn.starttls()
+            conn.login(user=EMAIL, password=PASSWORD)
+            conn.sendmail(
+                from_addr=EMAIL,
+                to_addrs="afaffaiyyeee",
+                msg=f"Subject:Ahoy!\n\n{message}"
+            )
