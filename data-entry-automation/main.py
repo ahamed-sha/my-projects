ec75fc30a3b71dff1d18538fdfb32d91a14fbed2
Ahamed Sha <73770368+ahamed-sha@users.noreply.github.com>
2021-07-06 20:45:03 +0530

    Update main.py

--- data-entry-automation/main.py
+++ data-entry-automation/main.py
@@ -4,7 +4,7 @@ import requests
 from time import sleep
 
 CHROME_PATH = "C:/Development/chromedriver.exe"
-FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSdNHeDepFra9atn97Bya7CJNQj387Ej6DHjTjWxlDwxxmlHLg/viewform?usp=sf_link"
+FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSdNHeDepFra9atn97Bya7JNQj387Ej6DjTjWxlDwxxmlHLg/viewform?usp=sf_link"
 ZILLOW_URL = "https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C%22mapBounds%22%3A%7B%22west%22%3A-122.70318068457031%2C%22east%22%3A-122.16347731542969%2C%22south%22%3A37.66421828296719%2C%22north%22%3A37.88619799953031%7D%2C%22mapZoom%22%3A11%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%2C%22fore%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22%3A%7B%22max%22%3A3000%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%7D"
 ZILLOW = "www.zillow.com"
 # ----------------------- BEAUTIFULSOUP PARSER / ZILLOW ----------------------- #
