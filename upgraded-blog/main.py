4f1ca166000bd9d81ff804dbc84010c76ba70622
Ahamed Sha <73770368+ahamed-sha@users.noreply.github.com>
2021-07-29 10:19:28 +0530

    Update main.py

--- upgraded-blog/main.py
+++ upgraded-blog/main.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
+
+USERNAME = "username"
+PASSWORD = "pass"
 
 response = requests.get(url="https://api.npoint.io/c5ce5730c0dfdc33fa14")
 blog_text = response.json()
@@ -17,9 +21,29 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/contact')
+def send_mail():
+    username = request.form['username']
+    email = request.form['email']
+    phone = request.form['phone']
+    message = request.form['message']
+    with smtplib.SMTP("smtp.gmail.com") as conn:
+        conn.starttls()
+        conn.login(user=USERNAME, password=PASSWORD)
+        conn.sendmail(
+            from_addr=USERNAME,
+            to_addrs=USERNAME,
+            msg=f"Subject:A mail from a fan!\n\nName:{username}\nMAIL_ID:{email}\nPhone:{phone}\nMessage:{message}"
+        )
+
+
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    return render_template('contact.html')
+    method = request.method
+    if method == 'POST':
+        send_mail()
+        return render_template('contact.html', method=method)
+    else:
+        return render_template('contact.html', method=method)
 
 
 @app.route('/post/<int:num>')
